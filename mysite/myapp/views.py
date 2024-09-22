e44893555245635ac5397d7f3755facde923b621
K0ndrich <123kondrich@gmail.com>
2024-09-22 18:41:19 +0300

    edited

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -12,5 +12,12 @@ def index(request):
     return render(request=request, template_name="myapp/index.html", context=context)
 
 
+# id значение передаеться пользователем в url-адрессе  # -> http://127.0.0.1:8000/myapp/hello/id/
+def indexItem(request, id):
+    item = Product.objects.get(id=id)
+    context = {"item": item}
+    return render(request=request, template_name="myapp/detail.html", context=context)
+
+
 def contacts(request):
     return render(request=request, template_name="myapp/contacts.html")

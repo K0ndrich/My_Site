82e98ad4f0b5d80a1e7bcac5e2e1aa1d7ba6af19
K0ndrich <123kondrich@gmail.com>
2024-09-27 18:42:29 +0300

    edited

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 
 # my_project
-from myapp.views import index, contacts , indexItem
+from myapp.views import index, contacts, indexItem
 
 urlpatterns = [
     path("hello/", index),  # -> http://127.0.0.1:8000/myapp/hello/
-    path("hello/<int:id>/", indexItem),  # -> http://127.0.0.1:8000/myapp/hello/2/
-    
+    path(
+        "hello/<int:id>/", indexItem, name="detail"
+    ),  # -> http://127.0.0.1:8000/myapp/hello/2/
     path("contacts/", contacts),  # -> http://127.0.0.1:8000/myapp/contacts/
 ]

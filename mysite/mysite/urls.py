d58bc46c1f225120126f078a0cf1e0a5e6f1e0ab
K0ndrich <123kondrich@gmail.com>
2024-10-12 15:25:20 +0300

    asdasd

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     # вход в дамин панель
     path("admin/", admin.site.urls),
-    path("myapp/", include("myapp.urls" ,namespace="myapp"))
-]
+    path("myapp/", include("myapp.urls", namespace="myapp")),
+] 

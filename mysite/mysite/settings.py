0f462ba661f71acdab7e0c37b55e96965d5dd0f9
K0ndrich <123kondrich@gmail.com>
2024-10-22 18:36:42 +0300

    asd

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,13 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
+
 STATIC_ROOT = BASE_DIR / "static"
 
 # указывает, где лежать наши медиа(фото,видео) файлы внутри проекта
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / 'media'
 # указываем url-адресс, по которому бдуме обрабатывать медиа файлы, которые лежат в MEDIA_ROOT
-MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

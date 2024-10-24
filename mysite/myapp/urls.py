d52bfbfa3135c5fcb205a37a52992adc072e68f8
K0ndrich <123kondrich@gmail.com>
2024-10-24 14:45:13 +0300

    asd

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,13 +2,14 @@
 from django.urls import path
 
 # my_project
-from myapp.views import index, indexItem
+from myapp.views import index, indexItem, add_item
 
 app_name = "myapp"
 
 urlpatterns = [
     path("", index),  # -> http://127.0.0.1:8000/myapp/
     path("<int:my_id>/", indexItem, name="detail"),  # -> http://127.0.0.1:8000/myapp/2/
+    path(
+        "add_item/", add_item, name="detail"
+    ),  # ->http://127.0.0.1:8000/myapp/add_item/
 ]
-
-
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -6,14 +6,21 @@ from django.http import HttpResponse
 from myapp.models import Product
 
 
+# главная страница сайта со всеми главынми товарами
 def index(request):
     items = Product.objects.all()
     context = {"items": items}
     return render(request=request, template_name="myapp/index.html", context=context)
 
 
+# страница для обозначений одной записи товара на сайте
 # id значение передаеться пользователем в url-адрессе  # -> http://127.0.0.1:8000/myapp/hello/id/
 def indexItem(request, my_id):
     item = Product.objects.get(id=my_id)
     context = {"item": item}
     return render(request=request, template_name="myapp/detail.html", context=context)
+
+
+# страница для добавление нового товара на сайт
+def add_item(request):
+    return render(request, template_name="myapp/additem.html")
